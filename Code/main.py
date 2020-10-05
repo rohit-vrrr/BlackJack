0cae2cdf5a287b77a0028d6bb6a525710f1c0a04
Rohit <vr.rohit03@gmail.com>
2020-10-05 17:49:10 +0530

    Added Hand and Chip class

--- Code/main.py
+++ Code/main.py
@@ -6,6 +6,8 @@ ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
 values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8,
           'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
 
+playing = True
+
 
 class Card:
     def __init__(self, suit, rank):
@@ -32,3 +34,51 @@ class Deck:
 
     def deal(self):
         return self.deck.pop()
+
+
+class Hand:
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+
+    def add_card(self, card):
+        # from Deck.deal() --> single Card(suit, rank)
+        self.cards.append(card)
+        self.value = values[card.rank]
+
+        # track aces
+        if card.rank == 'Ace':
+            self.aces += 1
+
+    def adjust_for_ace(self):
+
+        # if total value > 21 and i still have an ace
+        # then change my ace to be 1, instead of 11
+        while self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -= 1
+
+
+class Chip:
+    def __init__(self, total=100):
+        self.total = total
+        self.bet = 0
+
+    def win_bet(self):
+        self.total += self.bet
+
+    def lose_bet(self):
+        self.total -= self.bet
+
+
+test_deck = Deck()
+test_deck.shuffle()
+
+# Player
+test_player = Hand()
+# Deal 1 card from the deck Card(suit, rank)
+pulled_card = test_deck.deal()
+print(pulled_card)
+test_player.add_card(pulled_card)
+print(test_player.value)
