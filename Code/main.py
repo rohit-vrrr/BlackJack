fc494e88ff3ccccbc60df1eefcc791443b34cef6
Rohit <vr.rohit03@gmail.com>
2020-10-05 18:48:50 +0530

    minor change

--- Code/main.py
+++ Code/main.py
@@ -13,7 +13,6 @@ class Card:
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
-        self.value = values[rank]
 
     def __str__(self):
         return f'{self.rank} of {self.suit}'
